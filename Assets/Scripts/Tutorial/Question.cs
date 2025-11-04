02cc239c350e77a4e9d182ccf7b34f9ebecec522
minaaa101 <imfine509@naver.com>
2025-11-04 12:18:36 +0900

    튜토리얼 단과대 선택

---
+++ Assets/Scripts/Tutorial/Question.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+
+public class Question : MonoBehaviour
+{
+    public QuestionManager manager;
+    public GameObject nextQuestion;
+    public double[] optionScores;
+
+    public void OnAnswerSelected(int optionIndex)
+    {
+        double score = 0;
+        if (optionIndex < optionScores.Length)
+            score = optionScores[optionIndex];
+
+        manager.AddScore(score);
+
+        gameObject.SetActive(false);
+
+        if (nextQuestion != null)
+        {
+            nextQuestion.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("모든 질문 완료!");
+            manager.ShowResult();
+        }
+    }
+}
