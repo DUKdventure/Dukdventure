35a09301c7907569d6bb88797cf3c3ad9bf71678
minaaa101 <imfine509@naver.com>
2025-11-15 03:27:25 +0900

    [Feat][Player] Player 움직임 구현

---
+++ Assets/Scripts/PlayerMove/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 40, -40);
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        transform.position = target.position + offset;
+        transform.LookAt(target.position);
+    }
+}
